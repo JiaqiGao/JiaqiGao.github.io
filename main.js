3f3f4f49eefc16e77c93f57e008daea23392a82d
JiaqiGao <jiaqigao12@gmail.com>
2018-03-14 17:27:13 -0500

    ok

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ $(document).ready(function(){
     name.addEventListener("click", function(){
         if (name.innerHTML == "Jiaqi Gao"){
             name.innerHTML = "高家琪";
-            motivation.innerHTML = "\"我记忆其实不是很好，所以做个博客也许是个好主意\" -- 我，一月 2018"
+           // motivation.innerHTML = "\"我记忆其实不是很好，所以做个博客也许是个好主意\" -- 我，一月 2018"
         }else{
             name.innerHTML = "Jiaqi Gao";
-            motivation.innerHTML = "\"i'm pretty bad at remembering things so maybe it'll be a good idea to start a blog\" -- me, jan 2018";
         }
     })
 
