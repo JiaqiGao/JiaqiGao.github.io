be8bd03b4f52331a8a38a619f73ab69c12ce18f8
JiaqiGao <jiaqigao12@gmail.com>
2019-02-19 20:27:19 -0600

    404 error update

--- static/js/404error/js.js
+++ static/js/404error/js.js
@@ -10,38 +10,36 @@ DAT.Globe = function() {
   var y = -0.2;
 
   function init() {
-      camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.01, 10 )
+      camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.01, 10 );
       camera.position.z = 1
 
       scene = new THREE.Scene();
-
-      // var bgtexture = new THREE.TextureLoader().load( "static/images/space.jpg" );
 
       var bgcolor = new THREE.Color("white");
 
       scene.background = bgcolor;
 
       //Create a new ambient light
-      var light = new THREE.AmbientLight( 0x888888 )
-      scene.add( light )
+      var light = new THREE.AmbientLight( 0x888888 );
+      scene.add( light );
 
       //Create a new directional light
-      var light = new THREE.DirectionalLight( 0xfdfcf0, 1 )
-      light.position.set(20,10,20)
-      scene.add( light )
+      var light = new THREE.DirectionalLight( 0xfdfcf0, 1 );
+      light.position.set(20,10,20);
+      scene.add( light );
 
       //Create geometry and material
-      var geometry = new THREE.SphereGeometry(0.5, 32, 32)
-      var texture = new THREE.TextureLoader().load("static/images/earth.jpg")
+      var geometry = new THREE.SphereGeometry(0.5, 32, 32);
+      var texture = new THREE.TextureLoader().load("static/images/earth.jpg");
       var material = new THREE.MeshBasicMaterial( {
           map: texture
       } );
 
-      point = new THREE.Mesh(geometry)
+      point = new THREE.Mesh(geometry);
 
 
-      material.bumpMap = THREE.TextureLoader("static/images/earthbump.jpg")
-      material.bumpScale = 0.05
+      material.bumpMap = THREE.TextureLoader("static/images/earthbump.jpg");
+      material.bumpScale = 0.05;
 
 
       mesh = new THREE.Mesh( geometry, material );
@@ -53,31 +51,21 @@ DAT.Globe = function() {
 
       document.body.appendChild( renderer.domElement );
 
-
-
-
-
   }
 
 
   function positioning(event) {
       if(event.clientX > window.innerWidth/2 && x < 0)
-              x = 0.1
+              x += 0.1
       if(event.clientX < window.innerWidth/2 && x > 0)
-              x = -0.1
-      if(event.clientY > window.innerHeight/2 && y < 0)
-              y = 0.1
-      if(event.clientY < window.innerHeight/2 && y > 0)
-              y = -0.1
+              x += -0.1
   }
 
   function animate(e) {
       requestAnimationFrame( animate )
       document.addEventListener("click", positioning)
       mesh.rotation.x += 0.03 * x
-      mesh.rotation.y += 0.03 * y
       renderer.render( scene, camera )
-
   }
 
   init();
