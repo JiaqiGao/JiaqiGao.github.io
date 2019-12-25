f2f4d04403bbaa10d215a00ce2eb0923cf1f25ad
JiaqiGao <jiaqigao12@gmail.com>
2019-12-25 09:25:38 -0500

    wrong branch

--- main.js
+++ main.js
@@ -1,63 +1,41 @@
-$(document).ready(function(){
-    var name_desktop = document.getElementById("name_desktop");
-    var name_mobile = document.getElementById("name_mobile");
-    var motivation = document.getElementById("motivation");
-    var blog = document.getElementsByClassName("blog");
-    for (var j=0; j<blog.length; j+=1){
-        blog[j].addEventListener("click", function(){
-            for (var i=0; i<blog.length; i+=1) {
-                if (blog[i].innerHTML == "blog" || blog[i].innerHTML == "博客") {
-                    document.getElementById('home').style.display = 'none';
-                    document.getElementById('first').style.display = 'block';
-                    if (blog[i].innerHTML != "blog") {
-                        blog[i].innerHTML = "回";
-                    } else {
-                        blog[i].innerHTML = "return";
-                    }
-                } else {
-                    document.getElementById('home').style.display = 'block';
-                    document.getElementById('first').style.display = 'none';
-                    if (blog[i].innerHTML != "return") {
-                        blog[i].innerHTML = "博客";
-                    } else {
-                        blog[i].innerHTML = "blog";
-                    }
-                }
-            }
-        });
-    }
 
 
+var all_chin    = document.getElementsByClassName("chin");
+var all_eng     = document.getElementsByClassName("eng");
 
 
-    document.getElementById("translate").addEventListener("click", function(){
-        translate();
-    });
-
-
-});
-
-function translate(){
-    var eng_option = document.getElementById("option1");
-
-    var all_chin    = document.getElementsByClassName("chin");
-    var all_eng     = document.getElementsByClassName("eng")
-
-    if (!(eng_option.checked)){
-        for (var i = 0; i < all_chin.length; i++ ) {
-            all_chin[i].style.display = "none";
-        }
-        for (var j = 0; j < all_eng.length; j++ ) {
-            all_eng[j].style.display = "block";
-        }
+function translate(eng_option){
+    if (eng_option){
+        var eng_display = "block";
+        var chin_display = "none";
     }else{
-        for (var k = 0; k < all_chin.length; k++ ) {
-            all_chin[k].style.display = "block";
-        }
-        for (var l = 0; l < all_eng.length; l++ ) {
-            all_eng[l].style.display = "none";
-        }
+        var eng_display = "none";
+        var chin_display = "block";
     }
+
+    for (var i = 0; i < all_chin.length; i++ ) {
+        all_chin[i].style.display = chin_display;
+    }
+    for (var j = 0; j < all_eng.length; j++ ) {
+        all_eng[j].style.display = eng_display;
+    }
+
+    // if (eng_option != new_option){
+    //     eng_option = new_option;
+    //     var eng_display = "block";
+    //     var chin_display = "none";
+    //     if (!eng_option){
+    //         var eng_display = "none";
+    //         var chin_display = "block";
+    //     }
+    //     for (var i = 0; i < all_chin.length; i++ ) {
+    //         all_chin[i].style.display = eng_display;
+    //     }
+    //     for (var j = 0; j < all_eng.length; j++ ) {
+    //         all_eng[j].style.display = chin_display;
+    //     }
+    // }
+    
 }
 
 
@@ -81,9 +59,6 @@ function time() {
 
     if (eng_option){
         date.innerHTML = weekdays[wk] + " "
-
-        if (weekdays[wk] == "Wednesday")
-            date.innerHTML = "It's "+  date.innerHTML +"my dudes <br>";
         date.innerHTML += month + "/" + d.getDate() + "/" + d.getFullYear() + "  ";
     }else{
         date.innerHTML = "星期" + weekdays_chin[wk] + " "
@@ -98,6 +73,8 @@ function time() {
     if ((month == "12") && (d.getDate() == "16")){
         date.innerHTML = "Today marks my " + (d.getFullYear() - 1999).toString() + " year(s) of surviving in this world!";
     }
+
+    translate(eng_option);
        
     var t = setTimeout(time, 500);
 }
