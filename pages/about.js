d7e3e84f7ae567dc62ff2042d906d07a208ba47e
JiaqiGao <jiaqigao12@gmail.com>
2023-04-25 23:44:15 -0400

    new website

---
+++ pages/about.js
@@ -0,0 +1,293 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  List,
+  Menu,
+  Segment
+} from "semantic-ui-react";
+import React, { Component } from 'react'
+import styles from '../styles/Home.module.css'
+
+const navItems = [
+  {route: '/', name: 'home'},
+  {route: '/about', name: 'about'},
+  {route: '/blog', name: 'blog'},
+]
+
+
+class Donation extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+      return (
+        <div className={styles.card}>
+          <h3>{this.props.title}</h3>
+          <p>{this.props.description}</p>
+          <br/>
+          <br/><br/>
+          <label>{this.props.msg2}</label>
+        </div>
+      )
+  }
+}
+
+class Main extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+
+  render(){
+    return (
+      <div className={styles.container}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <title>Jiaqi Gao</title>
+          {/* <link rel="icon" href="/favicon.ico" /> */}
+        </Head>
+  
+        <main className={styles.main}>
+        {/* <Menu borderless inverted pointing color="blue">
+          <Container>
+            <Menu.Item header active>
+              Home
+            </Menu.Item>
+            <Menu.Item>New feature</Menu.Item>
+            <Menu.Item>Press</Menu.Item>
+            <Menu.Item>New hires</Menu.Item>
+            <Menu.Item>About</Menu.Item>
+          </Container>
+        </Menu> */}
+        <Grid container stackable>
+          <Grid.Row>
+            <Segment basic>
+              <Header as="h1" size="huge">
+                <Header.Content>Jiaqi Gao</Header.Content>
+                <Header.Subheader>
+                  Online portfoilio
+                </Header.Subheader>
+              </Header>
+            </Segment>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={11}>
+              <Header size="large" as="h2">
+                <Header.Content>Sample blog post</Header.Content>
+                <Header.Subheader>
+                  March 6, 2017 by <a href="#root">Jack</a>
+                </Header.Subheader>
+              </Header>
+              <Divider hidden />
+              <p>
+                This blog post shows a few different types of content that's
+                supported and styled with Semantic-UI. Basic typesetting, list,
+                and code are all supported.
+              </p>
+              <Divider />
+              <p>
+                Cum sociis natoque penatibus et magnis{" "}
+                <a href="#root">dis parturient montes</a>, nascetur ridiculus
+                mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam
+                venenatis vestibulum. Sed posuere consectetur est at lobortis.
+                Cras mattis consectetur purus sit amet fermentum.
+              </p>
+              <blockquote>
+                Curabitur blandit tempus porttitor. Nullam quis risus eget urna
+                mollis ornare vel eu leo. Nullam id dolor id nibh ultricies
+                vehicula ut id elit.
+              </blockquote>
+              <p>
+                Etiam porta sem malesuada magna mollis euismod. Cras mattis
+                consectetur purus sit amet fermentum. Aenean lacinia bibendum
+                nulla sed consectetur.
+              </p>
+              <Header as="h3" size="large">
+                Heading
+              </Header>
+              <p>
+                Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+                auctor. Duis mollis, est non commodo luctus, nisi erat porttitor
+                ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta
+                ac consectetur ac, vestibulum at eros.
+              </p>
+              <Header as="h3" size="medium">
+                Sub-heading
+              </Header>
+              <p>
+                Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus.
+              </p>
+              <Segment secondary>
+                <code>Example code block</code>
+              </Segment>
+              <p>
+                Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem
+                malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus
+                commodo, tortor mauris condimentum nibh, ut fermentum massa.
+              </p>
+              <Header size="medium">Sub-heading</Header>
+              <p>
+                Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Aenean lacinia bibendum nulla sed
+                consectetur. Etiam porta sem malesuada magna mollis euismod.
+                Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                condimentum nibh, ut fermentum massa justo sit amet risus.
+              </p>
+              <ul>
+                <li>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur et.
+                </li>
+                <li>Donec id elit non mi porta gravida at eget metus.</li>
+                <li>Nulla vitae elit libero, a pharetra augue.</li>
+              </ul>
+              <p>
+                Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae
+                elit libero, a pharetra augue.
+              </p>
+              <ol>
+                <li>Vestibulum id ligula porta felis euismod semper.</li>
+                <li>
+                  Cum sociis natoque penatibus et magnis dis parturient montes,
+                  nascetur ridiculus mus.
+                </li>
+                <li>
+                  Maecenas sed diam eget risus varius blandit sit amet non
+                  magna.
+                </li>
+              </ol>
+              <p>
+                Cras mattis consectetur purus sit amet fermentum. Sed posuere
+                consectetur est at lobortis.
+              </p>
+              <Divider hidden />
+              <Header size="large" as="h2">
+                <Header.Content>Another blog post</Header.Content>
+                <Header.Subheader>
+                  April 1, 2027 by <a href="#root">Mac</a>
+                </Header.Subheader>
+              </Header>
+              <Divider hidden />
+              <p>
+                Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare
+                sem lacinia quam venenatis vestibulum. Sed posuere consectetur
+                est at lobortis. Cras mattis consectetur purus sit amet
+                fermentum.
+              </p>
+              <blockquote>
+                Curabitur blandit tempus porttitor. Nullam quis risus eget urna
+                mollis ornare vel eu leo. Nullam id dolor id nibh ultricies
+                vehicula ut id elit.
+              </blockquote>
+              <p>
+                Etiam porta sem malesuada magna mollis euismod. Cras mattis
+                consectetur purus sit amet fermentum. Aenean lacinia bibendum
+                nulla sed consectetur.
+              </p>
+              <p>
+                Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+                auctor. Duis mollis, est non commodo luctus, nisi erat porttitor
+                ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta
+                ac consectetur ac, vestibulum at eros.
+              </p>
+              <Divider hidden />
+              <Header size="large" as="h2">
+                <Header.Content>New feature</Header.Content>
+                <Header.Subheader>
+                  Autumn 13, 2019 by <a href="#root">Semantic</a>
+                </Header.Subheader>
+              </Header>
+              <Divider hidden />
+              <p>
+                Cum sociis natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare
+                sem lacinia quam venenatis vestibulum. Sed posuere consectetur
+                est at lobortis. Cras mattis consectetur purus sit amet
+                fermentum.
+              </p>
+              <ul>
+                <li>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur et.
+                </li>
+                <li>Donec id elit non mi porta gravida at eget metus.</li>
+                <li>Nulla vitae elit libero, a pharetra augue.</li>
+              </ul>
+              <p>
+                Etiam porta sem malesuada magna mollis euismod. Cras mattis
+                consectetur purus sit amet fermentum. Aenean lacinia bibendum
+                nulla sed consectetur.
+              </p>
+              <p>
+                Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
+                auctor. Duis mollis, est non commodo luctus, nisi erat porttitor
+                ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta
+                ac consectetur ac, vestibulum at eros.
+              </p>
+              <Divider hidden />
+              
+            </Grid.Column>
+            <Grid.Column width={4} floated="right">
+              <Segment secondary>
+                <Header as="h4">About</Header>
+                <p>
+                  Etiam porta <i>sem malesuada magna mollis euismod</i>. Cras
+                  mattis consectetur purus sit amet fermentum. Aenean lacinia
+                  bibendum nulla sed consectetur.
+                </p>
+              </Segment>
+            
+              <Header as="h4">Elsewhere</Header>
+              <List>
+              {navItems.map(item => <List.Item><Link href={item.route}>{item.name}</Link></List.Item >)} 
+              </List>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+
+          
+          {/* <h1 className={styles.title}>
+            Jiaqi Gao
+          </h1>
+
+          <ul>
+            {navItems.map(item => <List.Item><Link href={item.route}>{item.name}</Link></List.Item >)} 
+          </ul> */}
+
+  
+        
+          {/* { this.state.start ?  : } */}
+          
+         
+          
+        </main>
+  
+        {/* <footer className={styles.footer}>
+          <p>
+            This <a href="https://github.com/JiaqiGao/JiaqiGao.github.io">website</a> was put together using Next.js and React. 
+          </p>
+        </footer> */}
+      </div>
+    )
+  }
+  
+}
+
+
+export default function Home() {
+  return (
+   <Main />
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,72 @@
 import Head from 'next/head'
-import React, { Component } from 'react'
+import React, { useState, useCallback, Component } from "react";
 import styles from '../styles/Home.module.css'
-import { Button, Checkbox, Container } from 'semantic-ui-react'
+import { Button, Divider } from 'semantic-ui-react'
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images"; 
+import photos from "../lib/images";
 
-class Donation extends Component {
+function randomizeGallery(images){
+  var randomize = Math.floor(Math.random() * images.length-1)+1;
+  return(images.slice(randomize, -1).concat(images.slice(0, randomize)))
+}
+
+function GalleryApp(gallery) {
+  const images = randomizeGallery(gallery["gallery"])
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+  return (
+    <div>
+      <Gallery photos={images} onClick={openLightbox} />
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={images.map(x => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </div>
+  );
+}
+
+class Bookmark extends Component {
   constructor(props){
     super(props);
-
-    this.handleCheck = this.handleCheck.bind(this);
-  }
-
-  handleCheck() {
-    this.props.onChange(this.props.qId, !this.props.checked);
   }
 
   render(){
-    if (this.props.control_type) {
-      return (
-        <div className={styles.card}>
-          <h3>{this.props.title}</h3>
-          <p>{this.props.description}</p>
-          <br/>
-          <Checkbox label={<label>{this.props.msg1}</label>} onClick={this.handleCheck} />
-          <br/><br/>
-          <label>{this.props.msg2}</label>
+      return ( 
+        <div className={styles.bookmark}>
+          { this.props.link ? 
+          <Button inverted basic color={this.props.color} size='big'>
+          <a target="_blank" rel="noopener noreferrer" href={this.props.link}>{this.props.text}</a>
+          </Button>
+           :
+          <Button inverted color={this.props.color} size='big' onClick={this.props.clicker}>
+            {this.props.text}
+        </Button>
+        }
+          
         </div>
       )
-    } else {
-      return (
-        <div className={styles.card}>
-          <h3>{this.props.title}</h3>
-          <p>{this.props.description}</p>
-          <br/>
-          <Checkbox label={<label>{this.props.msg1}</label>} onClick={this.handleCheck} checked={this.props.checked}/>
-          <br/><br/>
-          <Checkbox label={<label>{this.props.msg2}</label>} onClick={this.handleCheck} checked={!this.props.checked}/>
-        </div>
-      )
-    }
-    
   }
 }
 
@@ -48,284 +76,86 @@ class Main extends Component {
     this.state = {
       controlStatus: Math.random() < 0.5,
       start: false,
-      startTime: 0,
-      endTime: 0,
+      ceramics: false,
+      italy: false,
+      startTime: (new Date(Date.now())).toDateString(),
       disabledButton: true,
-      donationText: "Submit Donations",
-      eq1: true,
-      eq2: true,
-      eq3: true,
-      eq4: true,
-      eq5: true,
-      cq1: false,
-      cq2: false,
-      cq3: false,
-      cq4: false,
-      cq5: false,
-      totalDonations: 0,
-      queryString: "#"
     }
 
-    this.handleStart = this.handleStart.bind(this);
-    this.handleEnd = this.handleEnd.bind(this);
-    this.handleChange = this.handleChange.bind(this);  // checkboxes
+    this.handleItaly = this.handleItaly.bind(this);
+    this.handleCeramics = this.handleCeramics.bind(this);
+
+  }
+
+  handleCeramics(){
+    if (this.state.ceramics){
+      this.setState({
+        ceramics: false,
+      })
+    }else{
+      this.setState({
+        ceramics: true,
+        italy: false
+      })
+    }  
   }
   
-  handleStart(){
-    this.setState({
-      start: true,
-      startTime: Date.now()
-    })
-  }
-
-  handleEnd(){
-    var total = 0;
-    if (this.state.controlStatus){
-      if (this.state.cq1){
-        total += 20;
-      }
-      if (this.state.cq2){
-        total += 15;
-      }
-      if (this.state.cq3){
-        total += 27;
-      }
-      if (this.state.cq4){
-        total += 12;
-      }
-      if (this.state.cq5){
-        total += 16;
-      }
+  handleItaly(){
+    if (this.state.italy){
+      this.setState({
+        italy: false,
+      })
     }else{
-      if (this.state.eq1){
-        total += 20;
-      }
-      if (this.state.eq2){
-        total += 15;
-      }
-      if (this.state.eq3){
-        total += 27;
-      }
-      if (this.state.eq4){
-        total += 12;
-      }
-      if (this.state.eq5){
-        total += 16;
-      }
-    }
-    
-
-    // Create Qualtrics Query String
-    var link = "https://uchicago.co1.qualtrics.com/jfe/form/SV_dgV556kBNVvZVmC?";
-    link+="Experiment="
-    if (this.state.controlStatus){
-      link+="control"
-    }else{
-      link+="experiment";
-    }
-    link+="&TimeSpent="+((Date.now()-this.state.startTime)/1000).toString();
-    link+="&TotalDonations="+(total).toString();
-
-    this.setState({
-      endTime: Date.now(),
-      disabledButton: false,
-      donationText: "Thank you!",
-      totalDonations: total,
-      queryString: link
-    })
-  }
-
-  componentDidUpdate() {
-    console.log((this.state.endTime-this.state.startTime)/1000, this.state.totalDonations);
-  }
-
-  handleChange(qId, checked) {
-    if (qId == "cq1"){
-      this.setState({cq1: checked});
-    }
-    if (qId == "cq2"){
-      this.setState({cq2: checked});
-    }
-    if (qId == "cq3"){
-      this.setState({cq3: checked});
-    }
-    if (qId == "cq4"){
-      this.setState({cq4: checked});
-    }
-    if (qId == "cq5"){
-      this.setState({cq5: checked});
-    }
-    if (qId == "eq1"){
-      this.setState({eq1: checked});
-    }
-    if (qId == "eq2"){
-      this.setState({eq2: checked});
-    }
-    if (qId == "eq3"){
-      this.setState({eq3: checked});
-    }
-    if (qId == "eq4"){
-      this.setState({eq4: checked});
-    }
-    if (qId == "eq5"){
-      this.setState({eq5: checked});
-    }
+      this.setState({
+        italy: true,
+        ceramics: false
+      })
+    }  
   }
 
 
   render(){
+    
+
     return (
       <div className={styles.container}>
         <Head>
-          <title>Ethics Assignment 3</title>
+          <title>Jiaqi Gao</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
   
         <main className={styles.main}>
-          <h3 className={styles.personal}><a href="actually-me/index.html">If you are here for my personal website, click here please!</a> (Excuse my reuse of github pages haha!)</h3>
-          <br />
-          <h1 className={styles.title}>
-            Kirby's Campaign Donation Website
-          </h1>
-  
-          <p className={styles.description}>
-            This is a campaign donation portal for ✨Kirby✨, your preferred, beloved💕 candidate running in a local election!
-            <br/>You truly want to support Kirby's campaign, but you have an approximate donation budget of $40.
-          </p>
-
-          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/SSU_Kirby_artwork.png/220px-SSU_Kirby_artwork.png"/>
-            
-          <p className={styles.description}>
-            You will be asked to confirm your donation, then directed to fill out a quick survey.
-            <br/>Click the button below when you're ready. 
-            <br/>Disclaimer: This donation portal is totally fake and your real money is safe in your wallet (safe from this website at least).
-          </p>
-  
-          { this.state.start ? 
-
           <div>
-            {this.state.controlStatus ? 
-
-            // CONTROL OPTIONS
-            <div className={styles.grid, styles.start}>
-              <Donation title={"One-time donation"}
-                        msg1={"Donate $20"} 
-                        msg2={"Leave unchecked for no donation this time."}  
-                        onChange={this.handleChange} 
-                        qId="cq1" 
-                        checked={this.state.cq1}
-                        control_type={true}/>
-              
-              <Donation title={"Monthly donation"}
-                        msg1={"Donate another $3 at the end of each month until the election in September."}  
-                        msg2={"Leave unchecked for no monthly donation at this time."}   
-                        onChange={this.handleChange} 
-                        qId="cq2" 
-                        checked={this.state.cq2}
-                        control_type={true}/>
-              
-              <Donation title={"Birthday donation"}
-                        description={"Kirby is turning 29 in a few days on April 27th. Wish Kirby a happy birthday with a small donation!"}
-                        msg1={"Donate an additional $27 on April 29th"}  
-                        msg2={"Leave unchecked for no birthday donation at this time."}   
-                        onChange={this.handleChange} 
-                        qId="cq3" 
-                        checked={this.state.cq3}
-                        control_type={true}/>
-              
-              <Donation title={"Super Kirby Fan donation"}
-                        description={"Demonstrate yourself as Kirby's TOP grassroots supporter! This is your last chance to get your status of Super Kirby Fan!"}
-                        msg1={"Donate a one-time additional $12"}  
-                        msg2={"Leave unchecked for no Super Kirby Fan donation."}   
-                        onChange={this.handleChange} 
-                        qId="cq4" 
-                        checked={this.state.cq4}
-                        control_type={true}/>
-
-              <Donation title={"Secret Bonus donation"}
-                        description={"Congratulations! You've been selected to be special true Kirby supporter. Donate to secure your spot!"}
-                        msg1={"Make this a monthly reoccuring donation of $2 for the year."}  
-                        msg2={"Leave unchecked for no special true Kirby supporter status."}   
-                        onChange={this.handleChange} 
-                        qId="cq5" 
-                        checked={this.state.cq5}
-                        control_type={true}/>
-
-            </div> 
-           
-           : 
-
-            // EXPERIMENT OPTIONS
-            <div className={styles.grid, styles.start}>
-              <Donation title={"One-time donation"} 
-                        msg1={"Donate $20"}
-                        msg2={"I want Kirby to lose!"}
-                        onChange={this.handleChange} 
-                        qId="eq1" 
-                        checked={this.state.eq1}
-                        control_type={false}/>
-
-              <Donation title={"Monthly donation"} 
-                        msg1={"Donate $3 at the end of each month until the election in June."} 
-                        msg2={"Betray Kirby. Check this = Kirby loses the election."} 
-                        onChange={this.handleChange} qId="eq2" checked={this.state.eq2}
-                        control_type={false}/>
-              
-              <Donation title={"Birthday donation"}
-                        description={"Kirby is turning 29 in a few days on April 27th. Wish Kirby a happy birthday with a small donation!"}
-                        msg1={"Donate an additional $27 on April 29th"}  
-                        msg2={"You hope Kirby never gets to celebrate their 29th birthday 👀"}   
-                        onChange={this.handleChange} 
-                        qId="eq3" 
-                        checked={this.state.eq3}
-                        control_type={false}/>
-
-              <Donation title={"Super Kirby Fan donation"}
-                        description={"Demonstrate yourself as Kirby's TOP grassroots supporter! This is your last chance to get your status of Super Kirby Fan!"}
-                        msg1={"Donate a one-time additional $12"}  
-                        msg2={"If you check this box, we will HAVE TO tell Kirby you're a DEFECTOR & side with Meta Knight."}   
-                        onChange={this.handleChange} 
-                        qId="eq4" 
-                        checked={this.state.eq4}
-                        control_type={false}/>
-              
-              <Donation title={"Secret Bonus donation"}
-                        description={"Congratulations! You've been selected to be special true Kirby supporter. Donate to secure your spot!"}
-                        msg1={"Make this a monthly reoccuring donation of $2 for the year."}  
-                        msg2={"Miss out on this rare, LAST chance to join hands with Kirby and win the election."}   
-                        onChange={this.handleChange} 
-                        qId="eq5" 
-                        checked={this.state.eq5}
-                        control_type={false}/>
-            </div>
-
-            }
-
-            <br/>
-
-              <a href={this.state.queryString} target="_blank">
-                <Button disabled={this.state.disabledButton} color='orange' floated='right'>
-                  Take Survey!
-                </Button>
-              </a>
-            
-              <Button disabled={!this.state.disabledButton} inverted color='green' onClick={this.handleEnd} floated='right'>
-                  {this.state.donationText}
-              </Button>
-
+          <h1 className={styles.title}>
+            Jiaqi Gao
+          </h1>
           </div>
           
-            :
 
-          <Button inverted color='green' onClick={this.handleStart} size='big'>
-            Alrighty, let's starty.
-          </Button> }
+          <div className={styles.date}>
+           Date: {this.state.startTime}
+          </div>
+    
+          <p className={styles.description}>
+            </p>
+
+            <div className={styles.description}>
+            <Bookmark text="Ceramics" color="green" clicker={this.handleCeramics}/>
+            <Bookmark text="that one time i went to italy" color="blue" clicker={this.handleItaly}/>
+            <Bookmark text="instagram" color="yellow" link="https://www.instagram.com/jqig_/"/>
+            <Bookmark text="linkedin" color="yellow" link="https://www.linkedin.com/in/JiaqiGao1"/>
+            </div>
+       <Divider/>
+
+{ this.state.italy ? <GalleryApp gallery={photos["italy"]} /> : <br />}
+{ this.state.ceramics ? <GalleryApp gallery={photos["ceramics"]} /> : <br />}
+
           
         </main>
   
         <footer className={styles.footer}>
           <p>
-            This website was scratched together for a UChicago class assignment using Next.js and React. Thank you!
+            Want to reach me? Send me a message over on <a href="" >Instagram</a> or <a href="">LinkedIn</a>. 
           </p>
         </footer>
       </div>
